031504f02439d0919aca05999ccc637ba52f658e
natabgaf <natanfernandes52@gmail.com>
2023-08-02 11:54:11 -0300

    reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { GlobalStyle } from "./styles/GlobalStyled";
+import ResetCSS from "./styles/ResetCSS";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
+      <ResetCSS />
     </>
   );
 }
