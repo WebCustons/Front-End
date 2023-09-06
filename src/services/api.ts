b32838d5dfbebc3cbb2376541df2bdfa9ba6bc32
Christian <christianryuji@hotmail.com>
2023-09-06 01:38:05 -0300

    change url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-  // baseURL: "https://webcustons.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://webcustons.onrender.com",
+  // baseURL: "http://localhost:3000",
   timeout: 7000,
 })
 
